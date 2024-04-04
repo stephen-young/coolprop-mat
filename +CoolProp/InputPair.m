ac0ba8d3e43465fbca5c5e425ad51358dc77a10a
Stephen Young <sx4_young@laurentian.ca>
2024-04-04 16:56:14 -0400

    Refactor InputPair and Parameter classes
    InputPair and Parameter classes were originally written as enumeration
    classes. This requried that the index values to be hardcoded in order
    for them to be convertable to integers for use with the CoolProp dll
    functions. This would create a maintainability issue with requiring to
    maintain the integer values of the parameters and input pairs.
    
    Now the enumerations of the InputPair and Parameter classes have been
    changed to constant properties that query the index value from the dll
    functions. The values of indicies are evaluated whenever the first use
    of the class is done for more information see:
    https://www.mathworks.com/help/matlab/matlab_oop/expressions-in-class-definitions.html

--- +CoolProp/InputPair.m
+++ +CoolProp/InputPair.m
@@ -1,44 +1,44 @@
-classdef InputPair < int32
+classdef InputPair
     % Parameter indicies based on input_pairs enum at
     % https://github.com/CoolProp/CoolProp/blob/master/include/DataStructures.h#L274
-    % Not all parameters are currently included
 
-    enumeration
-        QT_INPUTS           (1) % Molar quality, Temperature in K
-        PQ_INPUTS           (2) % Pressure in Pa, Molar quality
-        QSmolar_INPUTS      (3) % Molar quality, Entropy in J/mol/K
-        QSmass_INPUTS       (4) % Molar quality, Entropy in J/kg/K
-        HmolarQ_INPUTS      (5) % Enthalpy in J/mol, Molar quality
-        HmassQ_INPUTS       (6) % Enthalpy in J/kg, Molar quality
-        DmolarQ_INPUTS      (7) % Density in mol/m^3, Molar quality
-        DmassQ_INPUTS       (8) % Density in kg/m^3, Molar quality
-        PT_INPUTS           (9) % Pressure in Pa, Temperature in K
-        DmassT_INPUTS       (10) % Mass density in kg/m^3, Temperature in K
-        DmolarT_INPUTS      (11) % Molar density in mol/m^3, Temperature in K
-        HmolarT_INPUTS      (12) % Enthalpy in J/mol, Temperature in K
-        HmassT_INPUTS       (13) % Enthalpy in J/kg, Temperature in K
-        SmolarT_INPUTS      (14) % Entropy in J/mol/K, Temperature in K
-        SmassT_INPUTS       (15) % Entropy in J/kg/K, Temperature in K
-        TUmolar_INPUTS      (16) % Temperature in K, Internal energy in J/mol
-        TUmass_INPUTS       (17) % Temperature in K, Internal energy in J/kg
-        DmassP_INPUTS       (18) % Mass density in kg/m^3, Pressure in Pa
-        DmolarP_INPUTS      (19) % Molar density in mol/m^3, Pressure in Pa
-        HmassP_INPUTS       (20) % Enthalpy in J/kg, Pressure in Pa
-        HmolarP_INPUTS      (21) % Enthalpy in J/mol, Pressure in Pa
-        PSmass_INPUTS       (22) % Pressure in Pa, Entropy in J/kg/K
-        PSmolar_INPUTS      (23) % Pressure in Pa, Entropy in J/mol/K
-        PUmass_INPUTS       (24) % Pressure in Pa, Internal energy in J/kg
-        PUmolar_INPUTS      (25) % Pressure in Pa, Internal energy in J/mol
-        HmassSmass_INPUTS   (26) % Enthalpy in J/kg, Entropy in J/kg/K
-        HmolarSmolar_INPUTS (27) % Enthalpy in J/mol, Entropy in J/mol/K
-        SmassUmass_INPUTS   (28) % Entropy in J/kg/K, Internal energy in J/kg
-        SmolarUmolar_INPUTS (29) % Entropy in J/mol/K, Internal energy in J/mol
-        DmassHmass_INPUTS   (30) % Mass density in kg/m^3, Enthalpy in J/kg
-        DmolarHmolar_INPUTS (31) % Molar density in mol/m^3, Enthalpy in J/mol
-        DmassSmass_INPUTS   (32) % Mass density in kg/m^3, Entropy in J/kg/K
-        DmolarSmolar_INPUTS (33) % Molar density in mol/m^3, Entropy in J/mol/K
-        DmassUmass_INPUTS   (34) % Mass density in kg/m^3, Internal energy in J/kg
-        DmolarUmolar_INPUTS (35) % Molar density in mol/m^3, Internal energy in J/mol
+    properties (Constant)
+
+        QT_INPUTS           = calllib(CoolProp.CoolProp.ALIAS, 'get_input_pair_index', 'QT_INPUTS')            % Molar quality, Temperature in K
+        PQ_INPUTS           = calllib(CoolProp.CoolProp.ALIAS, 'get_input_pair_index', 'PQ_INPUTS');           % Pressure in Pa, Molar quality
+        QSmolar_INPUTS      = calllib(CoolProp.CoolProp.ALIAS, 'get_input_pair_index', 'QSmolar_INPUTS');      % Molar quality, Entropy in J/mol/K
+        QSmass_INPUTS       = calllib(CoolProp.CoolProp.ALIAS, 'get_input_pair_index', 'QSmass_INPUTS');       % Molar quality, Entropy in J/kg/K
+        HmolarQ_INPUTS      = calllib(CoolProp.CoolProp.ALIAS, 'get_input_pair_index', 'HmolarQ_INPUTS');      % Enthalpy in J/mol, Molar quality
+        HmassQ_INPUTS       = calllib(CoolProp.CoolProp.ALIAS, 'get_input_pair_index', 'HmassQ_INPUTS');       % Enthalpy in J/kg, Molar quality
+        DmolarQ_INPUTS      = calllib(CoolProp.CoolProp.ALIAS, 'get_input_pair_index', 'DmolarQ_INPUTS');      % Density in mol/m^3, Molar quality
+        DmassQ_INPUTS       = calllib(CoolProp.CoolProp.ALIAS, 'get_input_pair_index', 'DmassQ_INPUTS');       % Density in kg/m^3, Molar quality
+        PT_INPUTS           = calllib(CoolProp.CoolProp.ALIAS, 'get_input_pair_index', 'PT_INPUTS');           % Pressure in Pa, Temperature in K
+        DmassT_INPUTS       = calllib(CoolProp.CoolProp.ALIAS, 'get_input_pair_index', 'DmassT_INPUTS');       % Mass density in kg/m^3, Temperature in K
+        DmolarT_INPUTS      = calllib(CoolProp.CoolProp.ALIAS, 'get_input_pair_index', 'DmolarT_INPUTS');      % Molar density in mol/m^3, Temperature in K
+        HmolarT_INPUTS      = calllib(CoolProp.CoolProp.ALIAS, 'get_input_pair_index', 'HmolarT_INPUTS');      % Enthalpy in J/mol, Temperature in K
+        HmassT_INPUTS       = calllib(CoolProp.CoolProp.ALIAS, 'get_input_pair_index', 'HmassT_INPUTS');       % Enthalpy in J/kg, Temperature in K
+        SmolarT_INPUTS      = calllib(CoolProp.CoolProp.ALIAS, 'get_input_pair_index', 'SmolarT_INPUTS');      % Entropy in J/mol/K, Temperature in K
+        SmassT_INPUTS       = calllib(CoolProp.CoolProp.ALIAS, 'get_input_pair_index', 'SmassT_INPUTS');       % Entropy in J/kg/K, Temperature in K
+        TUmolar_INPUTS      = calllib(CoolProp.CoolProp.ALIAS, 'get_input_pair_index', 'TUmolar_INPUTS');      % Temperature in K, Internal energy in J/mol
+        TUmass_INPUTS       = calllib(CoolProp.CoolProp.ALIAS, 'get_input_pair_index', 'TUmass_INPUTS');       % Temperature in K, Internal energy in J/kg
+        DmassP_INPUTS       = calllib(CoolProp.CoolProp.ALIAS, 'get_input_pair_index', 'DmassP_INPUTS');       % Mass density in kg/m^3, Pressure in Pa
+        DmolarP_INPUTS      = calllib(CoolProp.CoolProp.ALIAS, 'get_input_pair_index', 'DmolarP_INPUTS');      % Molar density in mol/m^3, Pressure in Pa
+        HmassP_INPUTS       = calllib(CoolProp.CoolProp.ALIAS, 'get_input_pair_index', 'HmassP_INPUTS');       % Enthalpy in J/kg, Pressure in Pa
+        HmolarP_INPUTS      = calllib(CoolProp.CoolProp.ALIAS, 'get_input_pair_index', 'HmolarP_INPUTS');      % Enthalpy in J/mol, Pressure in Pa
+        PSmass_INPUTS       = calllib(CoolProp.CoolProp.ALIAS, 'get_input_pair_index', 'PSmass_INPUTS');       % Pressure in Pa, Entropy in J/kg/K
+        PSmolar_INPUTS      = calllib(CoolProp.CoolProp.ALIAS, 'get_input_pair_index', 'PSmolar_INPUTS');      % Pressure in Pa, Entropy in J/mol/K
+        PUmass_INPUTS       = calllib(CoolProp.CoolProp.ALIAS, 'get_input_pair_index', 'PUmass_INPUTS');       % Pressure in Pa, Internal energy in J/kg
+        PUmolar_INPUTS      = calllib(CoolProp.CoolProp.ALIAS, 'get_input_pair_index', 'PUmolar_INPUTS');      % Pressure in Pa, Internal energy in J/mol
+        HmassSmass_INPUTS   = calllib(CoolProp.CoolProp.ALIAS, 'get_input_pair_index', 'HmassSmass_INPUTS');   % Enthalpy in J/kg, Entropy in J/kg/K
+        HmolarSmolar_INPUTS = calllib(CoolProp.CoolProp.ALIAS, 'get_input_pair_index', 'HmolarSmolar_INPUTS'); % Enthalpy in J/mol, Entropy in J/mol/K
+        SmassUmass_INPUTS   = calllib(CoolProp.CoolProp.ALIAS, 'get_input_pair_index', 'SmassUmass_INPUTS');   % Entropy in J/kg/K, Internal energy in J/kg
+        SmolarUmolar_INPUTS = calllib(CoolProp.CoolProp.ALIAS, 'get_input_pair_index', 'SmolarUmolar_INPUTS'); % Entropy in J/mol/K, Internal energy in J/mol
+        DmassHmass_INPUTS   = calllib(CoolProp.CoolProp.ALIAS, 'get_input_pair_index', 'DmassHmass_INPUTS');   % Mass density in kg/m^3, Enthalpy in J/kg
+        DmolarHmolar_INPUTS = calllib(CoolProp.CoolProp.ALIAS, 'get_input_pair_index', 'DmolarHmolar_INPUTS'); % Molar density in mol/m^3, Enthalpy in J/mol
+        DmassSmass_INPUTS   = calllib(CoolProp.CoolProp.ALIAS, 'get_input_pair_index', 'DmassSmass_INPUTS');   % Mass density in kg/m^3, Entropy in J/kg/K
+        DmolarSmolar_INPUTS = calllib(CoolProp.CoolProp.ALIAS, 'get_input_pair_index', 'DmolarSmolar_INPUTS'); % Molar density in mol/m^3, Entropy in J/mol/K
+        DmassUmass_INPUTS   = calllib(CoolProp.CoolProp.ALIAS, 'get_input_pair_index', 'DmassUmass_INPUTS');   % Mass density in kg/m^3, Internal energy in J/kg
+        DmolarUmolar_INPUTS = calllib(CoolProp.CoolProp.ALIAS, 'get_input_pair_index', 'DmolarUmolar_INPUTS'); % Molar density in mol/m^3, Internal energy in J/mol
 
     end
 
--- +CoolProp/Parameter.m
+++ +CoolProp/Parameter.m
@@ -1,88 +1,111 @@
-classdef Parameter < int32
+classdef Parameter
     % Parameter indicies based on parameters enum at
     % https://github.com/CoolProp/CoolProp/blob/master/include/DataStructures.h#L64
     % Not all parameters are currently included
 
-    enumeration
+    properties (Constant)
 
-        igas_constant                           (1)    % Ideal-gas constant
-        imolar_mass                             (2)    % Molar mass
-        iacentric_factor                        (3)    % Acentric factor
-        irhomolar_reducing                      (4)    % Molar density used for the reducing state
-        irhomolar_critical                      (5)    % Molar density used for the critical point
-        iT_reducing                             (6)    % Temperature at the reducing state
-        iT_critical                             (7)    % Temperature at the critical point
-        irhomass_reducing                       (8)    % Mass density at the reducing state
-        irhomass_critical                       (9)    % Mass density at the critical point
-        iP_critical                             (10)   % Pressure at the critical point
-        iP_reducing                             (11)   % Pressure at the reducing point
-        iT_triple                               (12)   % Triple point temperature
-        iP_triple                               (13)   % Triple point pressure
-        iT_min                                  (14)   % Minimum temperature
-        iT_max                                  (15)   % Maximum temperature
-        iP_max                                  (16)   % Maximum pressure
-        iP_min                                  (17)   % Minimum pressure
-        idipole_moment                          (18)   % Dipole moment
-        iT                                      (19)   % Temperature
-        iP                                      (20)   % Pressure
-        iQ                                      (21)   % Vapor quality
-        iTau                                    (22)   % Reciprocal reduced temperature
-        iDelta                                  (23)   % Reduced density
-        iDmolar                                 (24)   % Mole-based density
-        iHmolar                                 (25)   % Mole-based enthalpy
-        iSmolar                                 (26)   % Mole-based entropy
-        iCpmolar                                (27)   % Mole-based constant-pressure specific heat
-        iCp0molar                               (28)   % Mole-based ideal-gas constant-pressure specific heat
-        iCvmolar                                (29)   % Mole-based constant-volume specific heat
-        iUmolar                                 (30)   % Mole-based internal energy
-        iGmolar                                 (31)   % Mole-based Gibbs energy
-        iHelmholtzmolar                         (32)   % Mole-based Helmholtz energy
-        iHmolar_residual                        (33)   % The residual molar enthalpy
-        iSmolar_residual                        (34)   % The residual molar entropy (as a function of temperature and density)
-        iGmolar_residual                        (35)   % The residual molar Gibbs energy
-        iDmass                                  (36)   % Mass-based density
-        iHmass                                  (37)   % Mass-based enthalpy
-        iSmass                                  (38)   % Mass-based entropy
-        iCpmass                                 (39)   % Mass-based constant-pressure specific heat
-        iCp0mass                                (40)   % Mass-based ideal-gas specific heat
-        iCvmass                                 (41)   % Mass-based constant-volume specific heat
-        iUmass                                  (42)   % Mass-based internal energy
-        iGmass                                  (43)   % Mass-based Gibbs energy
-        iHelmholtzmass                          (44)   % Mass-based Helmholtz energy
-        iviscosity                              (45)   % Viscosity
-        iconductivity                           (46)   % Thermal conductivity
-        isurface_tension                        (47)   % Surface tension
-        iPrandtl                                (48)   % The Prandtl number
-        ispeed_sound                            (49)   % Speed of sound
-        iisothermal_compressibility             (50)   % Isothermal compressibility
-        iisobaric_expansion_coefficient         (51)   % Isobaric expansion coefficient
-        iisentropic_expansion_coefficient       (52)   % Isentropic expansion coefficient
-        ifundamental_derivative_of_gas_dynamics (53)   % The fundamental derivative of gas dynamics
-        ialphar                                 (54)
-        idalphar_dtau_constdelta                (55)
-        idalphar_ddelta_consttau                (56)
-        ialpha0                                 (57)
-        idalpha0_dtau_constdelta                (58)
-        idalpha0_ddelta_consttau                (59)
-        id2alpha0_ddelta2_consttau              (60)
-        id3alpha0_ddelta3_consttau              (61)
-        iBvirial                                (62)   % Second virial coefficient
-        iCvirial                                (63)   % Third virial coefficient
-        idBvirial_dT                            (64)   % Derivative of second virial coefficient with temperature
-        idCvirial_dT                            (65)   % Derivative of third virial coefficient with temperature
-        iZ                                      (66)   % The compressibility factor Z = p*v/(R*T)
-        iPIP                                    (67)   % The phase identification parameter of Venkatarathnam and Oellrich
-        ifraction_min                           (68)   % The minimum fraction (mole, mass, volume) for incompressibles
-        ifraction_max                           (69)   % The maximum fraction (mole,mass,volume) for incompressibles
-        iT_freeze                               (70)   % The freezing temperature for incompressibles
-        iGWP20                                  (71)   % The 20-year global warming potential
-        iGWP100                                 (72)   % The 100-year global warming potential
-        iGWP500                                 (73)   % The 500-year global warming potential
-        iFH                                     (74)   % Fire hazard index
-        iHH                                     (75)   % Health hazard index
-        iPH                                     (76)   % Physical hazard index
-        iODP                                    (77)   % Ozone depletion potential (R-11 = 1.0)
-        iPhase                                  (78)   % The phase index of the given state
+        % General parameters
+        igas_constant      = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'gas_constant')      % Molar gas constant in J/mol/K (O)
+        imolar_mass        = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'molar_mass')        % Molar mass in kg/mol (O)
+        iacentric_factor   = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'acentric')          % Acentric factor in - (O)
+        irhomolar_reducing = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'rhomolar_reducing') % Molar density at reducing point in mol/m^3 (O)
+        irhomolar_critical = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'rhomolar_critical') % Molar density at critical point in mol/m^3 (O)
+        iT_reducing        = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'T_reducing')        % Temperature at the reducing point in K (O)
+        iT_critical        = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'T_critical')        % Temperature at the critical point in K (O)
+        irhomass_reducing  = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'rhomass_reducing')  % Mass density at reducing point in kg/m^3 (O)
+        irhomass_critical  = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'rhomass_critical')  % Mass density at critical point in kg/m^3 (O)
+        iP_critical        = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'p_critical')        % Pressure at the critical point in Pa (O)
+        iP_reducing        = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'p_reducing')        % Pressure at the reducing point in Pa (O)
+        iT_triple          = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'T_triple')          % Temperature at the triple point in K (O)
+        iP_triple          = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'p_triple')          % Pressure at the triple point (pure only) in Pa (O)
+        iT_min             = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'T_min')             % Minimum temperature limit in K (O)
+        iT_max             = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'T_max')             % Maximum temperature limit in K (O)
+        iP_max             = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'P_max')             % Maximum pressure limit in Pa (O)
+        iP_min             = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'P_min')             % Minimum pressure limit in Pa (O)
+        idipole_moment     = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'dipole_moment')     % Dipole moment in C-m (O)
+
+        % Bulk properties
+        iT     = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'T')     % Temperature in K (IO)
+        iP     = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'P')     % Pressure in Pa (IO)
+        iQ     = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'Q')     % Molar vapor quality in mol/mol (IO)
+        iTau   = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'Tau')   % Reciprocal reduced temperature (Tc/T) in - (IO)
+        iDelta = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'Delta') % Reduced density (rho/rhoc) in - (IO)
+
+        % Molar specific thermodynamic properties
+        iDmolar          = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'Dmolar')          % Molar density in mol/m^3 (IO)
+        iHmolar          = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'Hmolar')          % Molar specific enthalpy in J/mol (IO)
+        iSmolar          = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'Smolar')          % Molar specific entropy in J/mol/K (IO)
+        iCpmolar         = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'Cpmolar')         % Molar specific constant pressure specific heat in J/mol/K (O)
+        iCp0molar        = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'Cp0molar')        % Ideal gas molar specific constant pressure specific heat in J/mol/K (O)
+        iCvmolar         = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'Cvmolar')         % Molar specific constant volume specific heat in J/mol/K (O)
+        iUmolar          = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'Umolar')          % Molar specific internal energy in J/mol (IO)
+        iGmolar          = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'Gmolar')          % Molar specific Gibbs energy in J/mol (O)
+        iHelmholtzmolar  = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'Helmholtzmolar')  % Molar specific Helmholtz energy in J/mol (O)
+        iHmolar_residual = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'Hmolar_residual') % Residual molar enthalpy in J/mol/K (O)
+        iSmolar_residual = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'Smolar_residual') % Residual molar entropy (sr/R = s(T,rho) - s^0(T,rho)) in J/mol/K (O)
+        iGmolar_residual = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'Gmolar_residual') % Residual molar Gibbs energy in J/mol/K (O)
+
+        % Mass specific thermodynamic properties
+        iDmass          = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'Dmass')         % Mass density in kg/m^3 (IO)
+        iHmass          = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'Hmass')         % Mass specific enthalpy in J/kg (IO)
+        iSmass          = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'Smass')         % Mass specific entropy in J/kg/K (IO)
+        iCpmass         = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'Cpmass')        % Mass specific constant pressure specific heat in J/kg/K (O)
+        iCp0mass        = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'Cp0mass')       % Ideal gas mass specific constant pressure specific heat in J/kg/K (O)
+        iCvmass         = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'Cvmass')        % Mass specific constant volume specific heat in J/kg/K (O)
+        iUmass          = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'Umass')         % Mass specific internal energy in J/kg (IO)
+        iGmass          = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'Gmass')         % Mass specific Gibbs energy in J/kg (O)
+        iHelmholtzmass  = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'Helmholtzmass') % Mass specific Helmholtz energy in J/kg (O)
+
+        % Transport properties
+        iviscosity       = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'viscosity')       % Viscosity in Pa-s (O)
+        iconductivity    = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'conductivity')    % Thermal conductivity in W/m/K (O)
+        isurface_tension = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'surface_tension') % Surface tension in N/m (O)
+        iPrandtl         = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'Prandtl')         % Prandtl number in - (O)
+
+        % Derivative-based terms
+        ispeed_sound                      = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'speed_of_sound')                   % Speed of sound in m/s (O)
+        iisothermal_compressibility       = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'isothermal_compressibility')       % Isothermal compressibility in 1/Pa (O)
+        iisobaric_expansion_coefficient   = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'isobaric_expansion_coefficient')   % Isobaric expansion coefficient in 1/K (O)
+        iisentropic_expansion_coefficient = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'isentropic_expansion_coefficient') % Isentropic expansion coefficient in - (O)
+
+        % Fundamental derivative of gas dynamics
+        ifundamental_derivative_of_gas_dynamics = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'fundamental_derivative_of_gas_dynamics') % Fundamental derivative of gas dynamics in - (O)
+
+        % Derivatives of the residual non-dimensionalized Helmholtz energy with respect to the EOS variables
+        ialphar                  = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'alphar')                  % Residual Helmholtz energy in - (O)
+        idalphar_dtau_constdelta = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'dalphar_dtau_constdelta') % Derivative of residual Helmholtz energy with tau in - (O)
+        idalphar_ddelta_consttau = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'dalphar_ddelta_consttau') % Derivative of residual Helmholtz energy with delta in - (O)
+
+        % Derivatives of the ideal-gas non-dimensionalized Helmholtz energy with respect to the EOS variables
+        ialpha0                    = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'alpha0')                    % Ideal Helmholtz energy in - (O)
+        idalpha0_dtau_constdelta   = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'dalpha0_dtau_constdelta')   % Derivative of ideal Helmholtz energy with tau in - (O)
+        idalpha0_ddelta_consttau   = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'dalpha0_ddelta_consttau')   % Derivative of ideal Helmholtz energy with delta in - (O)
+        id2alpha0_ddelta2_consttau = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'd2alpha0_ddelta2_consttau') % Second derivative of ideal Helmholtz energy with delta in - (O)
+        id3alpha0_ddelta3_consttau = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'd3alpha0_ddelta3_consttau') % Third derivative of ideal Helmholtz energy with delta in - (O)
+
+        % Other functions and derivatives
+        iBvirial     = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'Bvirial')     % Second virial coefficient in - (O)
+        iCvirial     = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'Cvirial')     % Third virial coefficient in - (O)
+        idBvirial_dT = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'dBvirial_dT') % Derivative of second virial coefficient with respect to T in - (O)
+        idCvirial_dT = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'dCvirial_dT') % Derivative of third virial coefficient with respect to T in - (O)
+        iZ           = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'Z')           % Compressibility factor in - (O)
+        iPIP         = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'PIP')         % Phase identification parameter in - (O)
+
+        % Accessors for incompressibles
+        ifraction_min = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'fraction_min') % Fraction (mole, mass, volume) minimum value for incompressible solutions in - (O)
+        ifraction_max = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'fraction_max') % Fraction (mole, mass, volume) maximum value for incompressible solutions in - (O)
+        iT_freeze     = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'T_freeze')     % Freezing temperature for incompressible solutions in K (O)
+
+        % Environmental parameters
+        iGWP20  = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'GWP20')  % 20-year global warming potential in - (O)
+        iGWP100 = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'GWP100') % 100-year global warming potential in - (O)
+        iGWP500 = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'GWP500') % 500-year global warming potential in - (O)
+        iFH     = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'FH')     % Flammability hazard in - (O)
+        iHH     = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'HH')     % Health hazard in - (O)
+        iPH     = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'PH')     % Physical hazard in - (O)
+        iODP    = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'ODP')    % Ozone depletion potential in - (O)
+        iPhase  = calllib(CoolProp.CoolProp.ALIAS, 'get_param_index', 'Phase')  % Phase index as a float in - (O)
 
     end
 
